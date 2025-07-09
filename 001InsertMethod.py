8e6e70cc1058acf63fc24577a3ad8a0f93e8c194
msrikarrao <mahendarkarsrikarrao@gmail.com>
2025-07-09 20:43:58 +0530

    Insert operation on lists

---
+++ 001InsertMethod.py
@@ -0,0 +1,8 @@
+cities = ["Delhi", 'Mumbai', 'Bengaluru']
+print(cities)
+cities.insert(1, "Chennai")
+print(cities)
+cities.insert(0, 'Hyderabad')
+print(cities)
+cities.insert(2, 'Pune')
+print(cities)
