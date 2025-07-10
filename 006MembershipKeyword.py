bcad6e2881fd3027681182f8710ae397e4b3630a
msrikarrao <mahendarkarsrikarrao@gmail.com>
2025-07-10 20:06:07 +0530

    In operator

---
+++ 006MembershipKeyword.py
@@ -0,0 +1,10 @@
+# Create list of 5 students
+students = ["M Srikar Rao", "G Sandeep", "G Pradeep", "Tharun", "Shasthri"]
+
+# Check if "G Pradeep" in students
+isGPradeepPresent = "G Pradeep" in students
+print(F"Is G Pradeep in the list of students? {"Yes" if isGPradeepPresent else "No"}.")
+
+# Check if "Vamshi" in students
+isVamshiPresent = "Vamshi" in students
+print(F"If Vamshi in the list of students? {"Yes" if isVamshiPresent else "No"}.")
---
+++ 007LoopOnList.py
@@ -0,0 +1,5 @@
+# Create a list of 7 cities in India
+cities = ["Mumbai", "Delhi", "Hyderabad", "Bengaluru", "Chennai", "Ranchi", "Ahmedabad"]
+
+for city in cities:
+    print(city)
