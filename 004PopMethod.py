7f4093073195ef92c04d55463b2628f47aba2f95
msrikarrao <mahendarkarsrikarrao@gmail.com>
2025-07-10 19:56:53 +0530

    Length function

--- 004PopMethod.py
+++ 004PopMethod.py
@@ -1,7 +1,17 @@
 # Create a list of 10 fruits
 fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon']
 
-# Remove lasr item using pop method
+# Remove last item using pop method
 lastItem = fruits.pop()
-print(lastItem)
-print(fruits)
+print(F"Last item removed : {lastItem}")
+print(F"Updated list : {fruits}\n")
+
+# Remove last item using pop method
+lastItem = fruits.pop()
+print(F"Last item removed : {lastItem}")
+print(F"Updated list : {fruits}\n")
+
+# Remove item at index 2 using pop method
+lastItem = fruits.pop(2)
+print(F"Item removed at index 2 : {lastItem}")
+print(F"Updated list : {fruits}\n")
---
+++ 005LenFunction.py
@@ -0,0 +1,5 @@
+# Create a list of 10 fruits
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon']
+
+# Get the length of the fruits list
+print(len(fruits))
