99418c41f1ceec2eb88ddc000e98b902483c3369
msrikarrao <mahendarkarsrikarrao@gmail.com>
2025-07-10 19:54:58 +0530

    Pop Method

---
+++ 003IndexMethod.py
@@ -0,0 +1,6 @@
+# Create a list with repeated names elements
+names = ["Alice", "Bob", "Charlie", "Alice", "Bob"]
+
+# Get the index number of "Alice"
+i = names.index('Bob')
+print(F"Bob's index : {i}")
---
+++ 004PopMethod.py
@@ -0,0 +1,7 @@
+# Create a list of 10 fruits
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon']
+
+# Remove lasr item using pop method
+lastItem = fruits.pop()
+print(lastItem)
+print(fruits)
